9dbefe08c75b688ebd4ec476d3d585662e3aa5c2
BigleBomb <irfanabyan96@gmail.com>
2017-10-09 23:22:18 +0700

    Initial commit

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -59,9 +59,9 @@ public class LineBotController
             }
         } else if (eventType.equals("message")){
             if (payload.events[0].source.type.equals("group")){
-                idTarget = payload.events[0].source.groupId;
+                idTarget = payload.events[0].source.userId;
             } else if (payload.events[0].source.type.equals("room")){
-                idTarget = payload.events[0].source.roomId;
+                idTarget = payload.events[0].source.userId;
             } else if (payload.events[0].source.type.equals("user")){
                 idTarget = payload.events[0].source.userId;
             }
